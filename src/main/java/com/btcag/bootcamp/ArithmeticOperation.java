c7b8c43530666f3913f1488560b02b47f8b4968e
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-17 15:12:26 +0200

    first commit

---
+++ src/main/java/com/btcag/bootcamp/ArithmeticOperation.java
@@ -0,0 +1,17 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int var = 3;
+        System.out.print(var);
+        System.out.print(++var); //pre-Inkrement. Erst wird um 1 erhöht, dann der erhöhte Wert verwendet;
+        System.out.print(var++); //post-Inkrement. Erst wird die Var. genutzt, danach um 1 erhöht
+
+
+
+
+    }
+}
