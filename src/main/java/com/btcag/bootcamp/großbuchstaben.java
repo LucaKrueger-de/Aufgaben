e02521a2fce6cecea01b6f50ff78f77dcc509747
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-20 10:40:18 +0200

    update

--- src/main/java/com/btcag/bootcamp/großbuchstaben.java
+++ src/main/java/com/btcag/bootcamp/großbuchstaben.java
@@ -6,7 +6,26 @@ public class großbuchstaben {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //einbelegtesbrotmitschinken
         String caps = input.nextLine();
-        System.out.println(caps.toUpperCase());
+
+        String output = "";
+
+        char reconversion;
+        int length = caps.length();
+        int counter = 0;
+
+        while (counter < length) {
+            char currChar = caps.charAt(counter);
+            int ascii = (int) currChar;
+            while (ascii > 90) {
+                ascii -= 32;
+            }
+            reconversion = (char) ascii;
+            output = output + reconversion;
+            counter++;
+        }
+
+        System.out.println(output);
+
 
     }
 }
---
+++ src/main/java/com/btcag/bootcamp/kryptischeZahlen.java
@@ -0,0 +1,21 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class kryptischeZahlen {
+    public static void main(String[] args) {
+        Scanner readIn = new Scanner (System.in);
+        System.out.println("Bitte die Zahl eingeben: ");
+        int number = readIn.nextInt();
+
+        System.out.println("Bitte Passwort (ZAHL) eingeben: ");
+        int pw = readIn.nextInt();
+
+        int fusion = number + pw;
+
+
+
+
+
+    }
+}
