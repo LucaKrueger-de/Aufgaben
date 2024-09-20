1b4248f3287176ddc5d091fecd69236b34a847fa
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-20 08:16:24 +0200

    ALLES GROß

---
+++ src/main/java/com/btcag/bootcamp/großbuchstaben.java
@@ -0,0 +1,12 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class großbuchstaben {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //einbelegtesbrotmitschinken
+        String caps = input.nextLine();
+        System.out.println(caps.toUpperCase());
+
+    }
+}
