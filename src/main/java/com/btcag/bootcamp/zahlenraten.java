7949a82378b6d3320dbef17c42c394df649038dd
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-23 13:41:43 +0200

    zahlenraten

---
+++ src/main/java/com/btcag/bootcamp/zahlenraten.java
@@ -0,0 +1,36 @@
+package com.btcag.bootcamp;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class zahlenraten {
+    public static void main(String[] args) {
+        Scanner readIn = new Scanner(System.in);
+        Random random = new Random();
+
+        int attempts = 1;
+        long guessNumber = 0;
+
+        System.out.println("Bitte gebe die Zahlengöße in Bits an: ");
+        int inBit = readIn.nextInt();
+        long bitShift = (long) 1 << inBit;
+        long maxNumb = bitShift - 1;
+        long randomNumb = random.nextLong(0, maxNumb);
+
+        while (guessNumber != randomNumb) {
+            System.out.println(attempts + ".Versuch: Bitte eine Zahl im Bereich 0 und " + maxNumb + " ein");
+            guessNumber = readIn.nextInt();
+            attempts++;
+            if (guessNumber < randomNumb) {
+                System.out.println("Die gesuchte Zahl ist größer! ");
+            } else if (guessNumber > randomNumb) {
+                System.out.println("Die gesuchte Zahl ist kleiner! ");
+
+            } else {
+                System.out.println("Super!");
+            }
+
+        }
+
+    }
+}
