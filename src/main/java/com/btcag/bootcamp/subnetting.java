2971dd46c7ad00b02d09482661814bc5182e6ce7
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-23 15:33:30 +0200

    zwischenstand

---
+++ src/main/java/com/btcag/bootcamp/subnetting.java
@@ -0,0 +1,29 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class subnetting {
+    public static void main(String[] args) {
+        Scanner readIn = new Scanner(System.in);
+        int i = 0;
+
+        System.out.println("Bitte gebe das erste Oktett der ip adresse an: ");
+        long oktett1 = readIn.nextLong();
+
+        System.out.println("Bitte gebe das zweite Oktett der ip adresse an: ");
+        long oktett2 = readIn.nextLong();
+        System.out.println("Bitte gebe das dritte Oktett der ip adresse an: ");
+        long oktett3 = readIn.nextLong();
+        System.out.println("Bitte gebe das vierte Oktett der ip adresse an: ");
+        long oktett4 = readIn.nextLong();
+        System.out.println("Bitte gebe die Subnetze an: ");
+        long subnetz = readIn.nextLong();
+
+        System.out.println("Die Netzwerkadresse ist: "+oktett1+"."+oktett2+"."+oktett3+"."+oktett4+ """
+                """);
+
+
+
+
+    }
+}
