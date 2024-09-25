b5cb2209f562987caee92f9a57c2b4a62419307c
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-25 13:46:51 +0200

    quadratische Zahlen

--- src/main/java/com/btcag/bootcamp/Aufgaben2/QuadratischeGleichungen.java
+++ src/main/java/com/btcag/bootcamp/Aufgaben2/QuadratischeGleichungen.java
@@ -1,4 +1,30 @@
 package com.btcag.bootcamp.Aufgaben2;
 
+import java.util.Scanner;
+
 public class QuadratischeGleichungen {
+    public static void main(String[] args) {
+        Scanner readIn = new Scanner(System.in);
+        System.out.println("Geben Sie einen Wert für [a] ein: ");
+        double a = readIn.nextDouble();
+        System.out.println("Geben Sie einen Wert für [b] ein: ");
+        double b = readIn.nextDouble();
+        System.out.println("Geben Sie einen Wert für [c] ein: ");
+        double c = readIn.nextDouble();
+
+        double diskriminante = (b*b) - 4 * a * c;
+
+
+        if (diskriminante > 0){
+            System.out.println("Die quadratische Gleichung hat zwei Lösungen, weil die Parabel die x-Achse an zwei Punkten schneidet. Ergebnis:"+diskriminante);
+        } else if (diskriminante == 0) {
+            System.out.print("Die quadratische Gleichung hat eine Lösung, weil die Parabel die x-Achse genau in einem Punkt berührt. Ergebnis:"+diskriminante);
+
+
+        }else {
+            System.out.println("Die quadratische Gleichung hat keine Lösung im Bereich der reellen Zahlen, weil die Parabel die x-Achse nicht schneidet. Ergebnis:"+diskriminante);
+        }
+
+    }
+
 }
