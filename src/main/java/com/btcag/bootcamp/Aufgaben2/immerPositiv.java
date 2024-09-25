6b4f32962a6a1bc4a7e27cbf3c6017d5933ec024
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-25 13:24:40 +0200

    immerPositiv

--- src/main/java/com/btcag/bootcamp/Aufgaben2/immerPositiv.java
+++ src/main/java/com/btcag/bootcamp/Aufgaben2/immerPositiv.java
@@ -1,7 +1,19 @@
 package com.btcag.bootcamp.Aufgaben2;
 
+import java.util.Scanner;
+
 public class immerPositiv {
     public static void main(String[] args) {
+        Scanner readIn = new Scanner(System.in);
+        System.out.println("Geben Sie eine Zahl ein: ");
+        int number = readIn.nextInt();
+
+        if (number < 0) {
+            int p = number * -1;
+            System.out.println(number + " --> " + p);
+        } else {
+            System.out.println(number + " --> " + number);
+        }
 
     }
 }
